39ec0fff96c9970c0917b6274c8552942b7d21c5
MD Jahidul Islam Jiku <safzahidul.cse@gmail.com>
2023-02-17 10:17:40 +0600

    useState object

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  // let [count, setCount] = useState(0);
-  // let [name, setName] = useState("");
-
+  // useState Object
   let [info, setInfo] = useState({
-    count: 0,
     name: "",
+    email: "",
+    password: "",
   });
 
-  let handleClick = () => {
-    info.count++;
-    // one-way
-    // setInfo({ ...info, count: info.count });
-    // two-way
-    setInfo((prev) => ({
-      ...prev,
-      count: info.count,
-    }));
-    console.log(info);
-  };
-
   let handleChange = (e) => {
-    // one-way
-    // setInfo({ ...info, name: e.target.value });
-    // two-way
+    let { name, value } = e.target;
     setInfo((prev) => ({
       ...prev,
-      name: e.target.value,
+      [name]: value,
     }));
     console.log(info);
   };
@@ -35,15 +20,54 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>useState LifeCycle Hooks</h1>
+        <h1>useState LifeCycle Hooks- Object</h1>
       </div>
-      <input onChange={handleChange} />
-      <h2>
-        {info.name}: {info.count}
-      </h2>
-      <button onClick={handleClick}>Click</button>
+      <div>
+        <h1>{info.name}</h1>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+        <input name="name" placeholder="name" onChange={handleChange} />
+        <input name="email" placeholder="email" onChange={handleChange} />
+        <input name="password" placeholder="password" onChange={handleChange} />
+      </div>
+      <button type="submit">Click</button>
     </div>
   );
+
+  // let handleClick = () => {
+  //   info.count++;
+  //   // one-way
+  //   // setInfo({ ...info, count: info.count });
+  //   // two-way
+  //   setInfo((prev) => ({
+  //     ...prev,
+  //     count: info.count,
+  //   }));
+  //   console.log(info);
+  // };
+  // let handleChange = (e) => {
+  //   // one-way
+  //   // setInfo({ ...info, name: e.target.value });
+  //   // two-way
+  //   setInfo((prev) => ({
+  //     ...prev,
+  //     name: e.target.value,
+  //   }));
+  //   console.log(info);
+  // };
+  // return (
+  //   <div className="App">
+  // <div>
+  //   <h1>{info.name}</h1>
+  // </div>;
+  //     <input onChange={handleChange} />
+  //     <h2>
+  //       {info.name}: {info.count}
+  //     </h2>
+  //     <button onClick={handleClick}>Click</button>
+  //   </div>
+  // );
 }
 
 export default App;
